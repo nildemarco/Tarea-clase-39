cffdaa84793dadce5480e415d2ddbe0460a71444
nildemarco <nildemarco@gmail.com>
2019-11-06 22:12:41 -0300

    agregando el for pero no funciona

--- for.js
+++ for.js
@@ -10,39 +10,52 @@
 // tijera gana a papel
 // 
 
-let opcionIngresadaUno = prompt(`Usuario 1: Elegi piedra, papel o tijera?`)
 
 
-if (opcionIngresadaUno == "piedra" || opcionIngresadaUno == "papel" || opcionIngresadaUno == "tijera") {
-    let opcionIngresadaDos = prompt(`Usuario 2: Elegi piedra, papel o tijera?`)
-    if (opcionIngresadaDos == "piedra" || opcionIngresadaDos == "papel" || opcionIngresadaDos == "tijera") {
-        if (opcionIngresadaDos == "piedra" && opcionIngresadaUno == "papel") {
-            alert(`Gano el usuario 1`)
+let puntosJugador1 = 0;
+let puntosJugador2 = 0;
+
+for (let i = 0; puntosJugador1 == 2 || puntosJugador2 == 2; i++) {
+    let opcionIngresadaUno = prompt(`Usuario 1: Elegi piedra, papel o tijera?`)
+    if (opcionIngresadaUno == "piedra" || opcionIngresadaUno == "papel" || opcionIngresadaUno == "tijera") {
+        let opcionIngresadaDos = prompt(`Usuario 2: Elegi piedra, papel o tijera?`)
+        if (opcionIngresadaDos == "piedra" || opcionIngresadaDos == "papel" || opcionIngresadaDos == "tijera") {
+            if (opcionIngresadaDos == "piedra" && opcionIngresadaUno == "papel") {
+                alert(`Gano el usuario 1`)
+                puntosJugador1++
+            }
+            else if (opcionIngresadaDos == "papel" && opcionIngresadaUno == "piedra") {
+                alert(` Gano el usuario 2 `)
+                puntosJugador2++
+            }
+            else if (opcionIngresadaDos == "tijera" && opcionIngresadaUno == "papel") {
+                alert(`Gano el usuario 2`)
+                puntosJugador2++
+            }
+            else if (opcionIngresadaDos == "tijera" && opcionIngresadaUno == "piedra") {
+                alert(`Gano el usuario 1`)
+                puntosJugador1++
+            }
+            else if (opcionIngresadaDos == "papel" && opcionIngresadaUno == "tijera") {
+                alert(`Gano el usuario 1`)
+                puntosJugador1++
+            }
+            else if (opcionIngresadaDos == "piedra" && opcionIngresadaUno == "tijera") {
+                alert(`Gano el usuario 2`)
+                puntosJugador2++
+            }
+            else if (opcionIngresadaDos == opcionIngresadaUno) {
+                alert(`Empate!`)
+            }
+
         }
-        else if (opcionIngresadaDos == "papel" && opcionIngresadaUno == "piedra") {
-            alert(` Gano el usuario 2 `)
+        else {
+            alert(`No podes jugar`)
         }
-        else if (opcionIngresadaDos == "tijera" && opcionIngresadaUno == "papel") {
-            alert(`Gano el usuario 2`)
-        }
-        else if (opcionIngresadaDos == "tijera" && opcionIngresadaUno == "piedra") {
-            alert(`Gano el usuario 1`)
-        }
-        else if (opcionIngresadaDos == "papel" && opcionIngresadaUno == "tijera") {
-            alert(`Gano el usuario 1`)
-        }
-        else if (opcionIngresadaDos == "piedra" && opcionIngresadaUno == "tijera") {
-            alert(`Gano el usuario 2`)
-        }
-        else if (opcionIngresadaDos == opcionIngresadaUno) {
-            alert(`Empate!`)
-        }
-        
-    }
-    else {
+
+    } else {
         alert(`No podes jugar`)
     }
 
-} else {
-    alert(`No podes jugar`)
 }
+
