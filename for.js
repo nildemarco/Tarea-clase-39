364886948f08305c69a37a802ff3fd094b91b1e8
nildemarco <nildemarco@gmail.com>
2019-11-06 21:40:53 -0300

    primera parte

---
+++ for.js
@@ -0,0 +1,48 @@
+// Hacer un programa que le permita a dos usuarios jugar al piedra, papel o tijera
+
+// El primer usuario tiene que ingresar su eleccion
+// Si no ingresa piedra, papel o tijera le decimos que no puede jugar
+// Despues el segundo usuario
+// Les respondemos quien gano .
+
+// piedra gana a tijera
+// papel gana piedra
+// tijera gana a papel
+// 
+
+let opcionIngresadaUno = prompt(`Usuario 1: Elegi piedra, papel o tijera?`)
+
+
+if (opcionIngresadaUno == "piedra" || opcionIngresadaUno == "papel" || opcionIngresadaUno == "tijera") {
+    let opcionIngresadaDos = prompt(`Usuario 2: Elegi piedra, papel o tijera?`)
+    if (opcionIngresadaDos == "piedra" || opcionIngresadaDos == "papel" || opcionIngresadaDos == "tijera") {
+        if (opcionIngresadaDos == "piedra" && opcionIngresadaUno == "papel") {
+            alert(`Gano el usuario 1`)
+        }
+        else if (opcionIngresadaDos == "papel" && opcionIngresadaUno == "piedra") {
+            alert(` Gano el usuario 2 `)
+        }
+        else if (opcionIngresadaDos == "tijera" && opcionIngresadaUno == "papel") {
+            alert(`Gano el usuario 2`)
+        }
+        else if (opcionIngresadaDos == "tijera" && opcionIngresadaUno == "piedra") {
+            alert(`Gano el usuario 1`)
+        }
+        else if (opcionIngresadaDos == "papel" && opcionIngresadaUno == "tijera") {
+            alert(`Gano el usuario 1`)
+        }
+        else if (opcionIngresadaDos == "piedra" && opcionIngresadaUno == "tijera") {
+            alert(`Gano el usuario 2`)
+        }
+        else if (opcionIngresadaDos == opcionIngresadaUno) {
+            alert(`Empate!`)
+        }
+        
+    }
+    else {
+        alert(`No podes jugar`)
+    }
+
+} else {
+    alert(`No podes jugar`)
+}
